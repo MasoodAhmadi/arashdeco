19db50eede2a75f0f74260e9d3f455382a40bdc5
Masood <masood.ahmadi@courses.tamk.fi>
2020-04-03 02:04:48 +0300

    added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Myclass from './pages/main.componen';
+
+function App() {
+  return (
+    <div className="App">
+     <Myclass/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/main.componen.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Myclass extends React.Component {
+    render(){
+        return (
+            <div id="main_wrapper">
+                    <header>
+                    <div id="main_title">
+                    <img src="C:\Users\Ahmad\Desktop\Third Semester\Practicing_web _Dev\images/arash_logo.png" alt="Logo de Zozor" id="logo" />
+                <h1>Decoration experienced</h1>
+                <h2>Decorating your wedding, birthday & gathering party at your door.</h2>
+                    </div>
+                <nav>
+                <ul class="ex1">
+                <li> home</li>
+                </ul>
+                </nav>
+                </header>
+                </div>
+    
+        )
+    }
+}
+ export default Myclass;
